1a8232c632f2abba47d1124b8e4e75918f9188e9
Muhammad Ibrahim <m.iibrahim9078@gmail.com>
2025-07-10 15:13:29 +0500

    Create Full working Quiz App

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
-let print = document.getElementById("print");
+  let questions = [], index = 0, score = 0;
+  const quizBox = document.getElementById("quizBox");
+  const nextBtn = document.getElementById("nextBtn");
+  const scoreBox = document.getElementById("scoreBox");
 
-fetch('https://the-trivia-api.com/v2/questions')
-.then(data => data.json())
-.then((data) => {
-    console.log(data)
-    console.log(data[0])
+  fetch('https://the-trivia-api.com/v2/questions?limit=10')
+    .then(res => res.json())
+    .then(data => {
+      questions = data;
+      loadQuestion();
+    });
 
-    for(i=0; i < data.length; i++){
+  function loadQuestion() {
+    nextBtn.disabled = true;
 
-        print.innerHTML = ` <div>
-                                <h2>${data[i].question.text}</h2>
-                                <p>◆ ${data[i].incorrectAnswers[0]}</p>
-                                <p>◆ ${data[i].incorrectAnswers[1]}</p>
-                                <p>◆ ${data[i].incorrectAnswers[2]}</p>
-                                <p>◆ ${data[i].correctAnswer}</p>
-                            </div>`
+    const q = questions[index];
+    const allOptions = [...q.incorrectAnswers, q.correctAnswer].sort(() => Math.random() - 0.5);
+
+    quizBox.innerHTML = `
+      <h4>Q${index + 1}: ${q.question.text}</h4>
+      <div class="d-grid gap-2 col-6 mx-auto mt-3">
+        ${allOptions.map(opt => `<button class="btn btn-outline-light" onclick="checkAnswer(this, '${q.correctAnswer.replace(/'/g, "\\'")}')">${opt}</button>`).join('')}
+      </div>
+    `;
+
+    nextBtn.classList.remove("d-none");
+  }
+
+  function checkAnswer(btn, correct) {
+    const allBtns = document.querySelectorAll(".btn-outline-light");
+    allBtns.forEach(b => b.disabled = true);
+
+    if (btn.innerText === correct) score++;
+
+    nextBtn.disabled = false;
+  }
+
+  function nextQuestion() {
+    index++;
+    if (index < questions.length) {
+      loadQuestion();
+    } else {
+      quizBox.innerHTML = "";
+      nextBtn.classList.add("d-none");
+      scoreBox.innerText = `You scored ${score} out of ${questions.length}`;
+      if(score === 3){
+        alert("Congragulation your passed because Your Percentage 30 Above")
+      }
     }
-   
-
-})
-
-
+  }
